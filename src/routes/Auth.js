e1d56942671b275d8d2e3ec3f98ac0ad7969a566
dove209 <dove209@gmail.com>
2022-02-04 16:13:12 +0900

    firebase authentication 적용

---
+++ src/routes/Auth.js
@@ -0,0 +1,94 @@
+import { authService } from "fbase";
+import React, { useState } from "react";
+import { firebaseInstance } from "fbase";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        // log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount(!newAccount);
+
+  const onSocialClick = async (event) => {
+    const { name } = event.target;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      {error}
+      <span onClick={toggleAccount}>
+        {newAccount ? "Log in" : "Create Account"}
+      </span>
+      <div>
+        <button name="google" onClick={onSocialClick}>
+          With Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          With Github
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
